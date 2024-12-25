a3c15a6e5c80aead50d72fa4814ac725b43b2006
manishjogi <gs.manishjogi@gmail.com>
2024-12-25 13:14:38 +0530

    5.entry point created in Hazel engine side

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//this is for use by hazel apps
+
+#include "Hazel/Application.h"
+
+//--------Entry Point-------------
+#include "Hazel/EntryPoint.h"
+
+//--------------------------------
