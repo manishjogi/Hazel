982cae0ac81ef892909317acb3b0123e9a4e5368
manishjogi <gs.manishjogi@gmail.com>
2024-12-25 16:17:00 +0530

    6.added logging- used submodule https://github.com/gabime/spdlog

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 //this is for use by hazel apps
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 //--------Entry Point-------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+
+	HZ_CORE_WARN("Initialized core Log!");
+	HZ_INFO("Intialized client information");
+
 	auto app = Hazel::CreateApplication();
 
 	app->Run();
